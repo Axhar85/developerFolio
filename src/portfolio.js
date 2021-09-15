c27d70100871fa881facf9a9dda0a53ff32badbc
Axhar85 <azhararshad_ntu@yahoo.com>
2021-09-15 22:00:20 +0200

    added blog

--- src/portfolio.js
+++ src/portfolio.js
@@ -32,6 +32,7 @@ const socialMediaLinks = {
   facebook: "https://www.facebook.com/azhar.arshad.94/",
   codewars: "https://www.codewars.com/users/Axhar85",
   instagram: "https://www.instagram.com/axhar/",
+  medium: "https://medium.com/@azhararshad_ntu",
   // Instagram and Twitter are also supported in the links!
   display: true // Set true to display this section, defaults to false
 };
@@ -299,20 +300,15 @@ const blogSection = {
     "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
 
   blogs: [
+  
     {
-      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
-      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      url: "https://medium.com/@azhararshad_ntu/difference-between-relational-and-non-relational-databases-def9a9217b2a",
+      title: "Difference between Relational and Non-relational Databases",
       description:
-        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
-    },
-    {
-      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
-      title: "Why REACT is The Best?",
-      description:
-        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+        "Database innovation has changed and advanced over the years. Relational, Non Relational, Cloud databases, hierarchical…it can begin to urge confusing. Storing information doesn’t have to be be a problem."
     }
   ],
-  display: false // Set false to hide this section, defaults to true
+  display: true // Set false to hide this section, defaults to true
 };
 
 // Talks Sections
