9c79f1cc3d8723957052271998d9e03221cc319e
Axhar85 <azhararshad_ntu@yahoo.com>
2021-10-10 14:23:53 +0200

    updated cv

--- src/portfolio.js
+++ src/portfolio.js
@@ -17,7 +17,7 @@ const greeting = {
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
   resumeLink:
-  "https://drive.google.com/file/d/1OW5larM-OwfCQcDRDZdQFj09V-Zcry00/view?usp=sharing",
+  "https://drive.google.com/file/d/1cNyRhLD3Uf-IjNOmHqPcsI4gNPfph9xX/view?usp=sharing",
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
