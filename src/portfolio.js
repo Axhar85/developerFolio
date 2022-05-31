9741b51e7387a5d6fb7bf48c00a94b8a4f9a25a1
Axhar <azhar-arshad_student2021@wilder.school>
2022-05-31 11:45:12 +0200

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -17,7 +17,7 @@ const greeting = {
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native / Vuejs and some other cool libraries and frameworks."
   ),
   resumeLink:
-  "https://drive.google.com/file/d/1cNyRhLD3Uf-IjNOmHqPcsI4gNPfph9xX/view?usp=sharing",
+  "https://drive.google.com/file/d/1TWcwjW61o6tfpXdS9KtQdHR-LM6GJqZU/view?usp=sharing",
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
